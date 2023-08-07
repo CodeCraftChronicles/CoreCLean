ee401007b0a3da1da896e069177b7e4f6047f9b6
Daniel WOAGOU <daniwx@gmail.com>
2023-08-07 15:30:15 +0000

    Adding entities

---
+++ Domain/Contracts/IEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreClean.BCL.Domaine.Contracts;
+public interface IEntity
+{
+}
+
+public interface IEntity<TId> : IEntity
+{
+    public TId Id { get; set; }
+}
+
+public interface IAuditableEntity : IEntity
+{
+    string CreatedBy { get; set; }
+
+    DateTime CreatedOn { get; init; } 
+
+    string? LastModifiedBy { get; set; }
+
+    DateTime? LastModifiedOn { get; set; }
+    bool IsDeleted { get; set; }
+    DateTime? DeletedOn { get; set; }
+    public string? DeletedBy { get; set; }
+}
+
+public interface IAuditableEntity<TId> : IAuditableEntity, IEntity<TId>
+{
+}
---
+++ Domain/Models/AuditableEntity.cs
@@ -0,0 +1,21 @@
+﻿using CoreClean.BCL.Domaine.Contracts;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreClean.BCL.Domaine.Models;
+
+public abstract class AuditableEntity<TId> : IAuditableEntity<TId>
+{       
+    public TId Id { get; set; }
+    public string CreatedBy { get; set; } = string.Empty;
+    public DateTime CreatedOn { get; init; } = DateTime.UtcNow;
+    public string? LastModifiedBy { get; set; } = null;
+    public DateTime? LastModifiedOn { get; set; } = null;    
+    public bool IsDeleted { get; set; } =default;
+    public DateTime? DeletedOn { get; set; }
+    public string? DeletedBy { get; set; }
+}
