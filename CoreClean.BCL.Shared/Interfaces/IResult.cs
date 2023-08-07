e5d79880b48ea7704e707d83ecb909fa98d424dd
Daniel WOAGOU <daniwx@gmail.com>
2023-08-07 17:14:06 +0000

    Creation de l'encapsuleur des responses

---
+++ CoreClean.BCL.Shared/Interfaces/IResult.cs
@@ -0,0 +1,21 @@
+﻿namespace CoreClean.BCL.Shared.Interfaces;
+
+public interface IDetailedMessage
+{
+    string Message { get; set; } 
+    string? Link { get; set; }
+}
+
+public interface IResult
+{
+    List<IDetailedMessage> Messages { get; set; }
+
+    bool Succeeded { get; set; }
+}
+
+public interface IResult<out T> : IResult
+{
+    T Data { get; }
+}
+
+
