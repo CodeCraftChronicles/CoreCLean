85bf69ff400162dbb31624a98e6da45ed240eac8
Daniel WOAGOU <daniwx@gmail.com>
2023-08-08 10:52:13 +0000

    Liaison des modules et debut de conception t4un outils de configuration base sur les fichiers Json

---
+++ CoreClean.BCL.Application/Configurations/AppConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace CoreClean.BCL.Application.Configurations;
+public class AppConfiguration
+{
+    public string Secret { get; set; } = String.Empty;
+    public bool BehindSSLProxy { get; set; } = false;
+    public string ProxyIP { get; set; } = String.Empty;
+    public string ApplicationUrl { get; set; } = String.Empty;
+    public string ApplicationName { get; set; } = String.Empty;
+    public string ApplicationVersion { get; set; } = String.Empty;
+    public string ConfigFilesRepository { get; set; } = string.Empty;
+}
---
+++ CoreClean.BCL.Infra.Infrastructure/Configurations/AppBuilderCollectionExtension.cs
@@ -0,0 +1,19 @@
+﻿using CoreClean.BCL.Application.Configurations;
+
+using Microsoft.Extensions.Configuration;
+namespace CoreClean.BCL.Infra.Infrastructure.Configurations
+{
+    public static class AppBuilderCollectionExtension
+    {
+        public static IConfigurationBuilder AddMultipleJsonFiles(this IConfigurationBuilder configurationBuilder, AppConfiguration appConfiguration, string prefix = "")
+        {
+            string[] files = Directory.GetFiles(appConfiguration.ConfigFilesRepository, prefix + "*.json");
+
+            foreach (var item in files)
+            {
+                configurationBuilder.AddJsonFile(item);
+            }
+            return configurationBuilder;
+        }
+    }
+}
